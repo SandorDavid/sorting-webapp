3b581387a1bbc7220743e44d3ff9c7fc88e44b11
Sandor David Olasz <olaszsandordavid@gmail.com>
2019-12-30 20:12:44 +0100

    Improved sorting test cases
    Removed String test case, because multiple tests of implementations of Comparable interface are redundant.
    Added several extra test cases

--- src/test/java/com/sandordavid/SortingWebapp/domain/sorting/SorterTest.java
+++ src/test/java/com/sandordavid/SortingWebapp/domain/sorting/SorterTest.java
@@ -5,11 +5,13 @@ import com.sandordavid.SortingWebapp.domain.sorting.impls.MergeSorter;
 import com.sandordavid.SortingWebapp.domain.sorting.impls.QuickSorter;
 import com.sandordavid.SortingWebapp.domain.sorting.impls.SelectionSorter;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
@@ -20,23 +22,32 @@ import static org.junit.Assert.assertEquals;
 @RunWith(value = Parameterized.class)
 public class SorterTest {
 
-    Class sorterImplClass;
+    private Class sorterImplClass;
+    private Sorter<Integer> sorter;
 
     public SorterTest(Class sorterImplClass) {
         this.sorterImplClass = sorterImplClass;
     }
 
-    @Test
-    public void sortingShortListOfStringsIsWorking() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
-        Sorter<String> sorter = (Sorter<String>) this.sorterImplClass.getDeclaredConstructor().newInstance();
-        List<String> unorderedList = Arrays.asList("adbb",",x","fasppg","a","bbb","aaa");
-        List<String> orderedList = Arrays.asList(",x", "a", "aaa", "adbb", "bbb", "fasppg");
-        assertEquals(orderedList, sorter.sort(unorderedList));
+    @Before
+    public void setup() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        sorter = (Sorter<Integer>) sorterImplClass.getDeclaredConstructor().newInstance();
     }
 
     @Test
-    public void sortingShortListOfIntegersIsWorking() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
-        Sorter<Integer> sorter = (Sorter<Integer>) this.sorterImplClass.getDeclaredConstructor().newInstance();
+    public void sortingEmptyListIsWorking(){
+        List<Integer> emptyList = new ArrayList<>();
+        assertEquals(emptyList, sorter.sort(emptyList));
+    }
+
+    @Test
+    public void sortingListWithOneElementIsWorking(){
+        List<Integer> oneElementList = new ArrayList<Integer>(){{add(2);}};
+        assertEquals(oneElementList, sorter.sort(oneElementList));
+    }
+
+    @Test
+    public void sortingShortListOfIntegersIsWorking() {
         List<Integer> unorderedList = Arrays.asList(4, 2, 643, 5, 23, 4, 523, 4, -462626, 5235, 132, 77, -12, 412);
         List<Integer> orderedList = Arrays.asList(-462626, -12, 2, 4, 4, 4, 5, 23, 77, 132, 412, 523, 643, 5235);
         assertEquals(orderedList, sorter.sort(unorderedList));
