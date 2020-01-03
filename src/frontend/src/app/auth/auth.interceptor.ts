50b95ea596f16564ba7272c2a95da34e86e6b94f
Sandor David Olasz <olaszsandordavid@gmail.com>
2020-01-03 12:56:29 +0100

    [ANGULAR] Fixing interceptor using the store

--- src/frontend/src/app/auth/auth.interceptor.ts
+++ src/frontend/src/app/auth/auth.interceptor.ts
@@ -1,25 +1,36 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, pipe } from 'rxjs';
+import { take, switchMap } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
+
+import { AppState } from '../store/app.reducer';
+import { AuthState } from './store/auth.reducers';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
+    constructor(private store: Store<AppState>){}
+
     intercept(req: HttpRequest<any>,
               next: HttpHandler): Observable<HttpEvent<any>> {
 
-        const idToken = localStorage.getItem('id_token');
-
-        if (idToken) {
-            const cloned = req.clone({
-                headers: req.headers.set('Authorization',
-                    'Bearer ' + idToken)
-            });
-
-            return next.handle(cloned);
-        }
-        else {
-            return next.handle(req);
-        }
-    }
+        return this.store.select('auth')
+            .pipe(
+                take(1),
+                switchMap((authState: AuthState) => {
+                    if (authState.token) {
+                        const cloned = req.clone({
+                            headers: req.headers.set('Authorization',
+                                'Bearer ' + authState.token)
+                        });
+            
+                        return next.handle(cloned);
+                    }
+                    else {
+                        return next.handle(req);
+                    }
+                })
+            )
+    }       
 }
