8043a99de0ce4d4082605dbbbbfd72f18766d998
Sandor David Olasz <olaszsandordavid@gmail.com>
2020-01-08 23:10:02 +0100

    Heroku fix bean

--- backend/src/main/java/com/sandordavid/SortingWebapp/Application.java
+++ backend/src/main/java/com/sandordavid/SortingWebapp/Application.java
@@ -2,18 +2,10 @@ package com.sandordavid.SortingWebapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        System.err.println("Hello, logs!");
         SpringApplication.run(Application.class, args);
     }
-
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-//        return builder.sources(Application.class);
-//    }
 }
