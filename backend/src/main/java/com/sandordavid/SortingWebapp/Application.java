abcd8521a67afe2f2278015440101ffbb30aba24
Sandor David Olasz <olaszsandordavid@gmail.com>
2020-01-08 22:01:23 +0100

    Heroku App properties update

--- backend/src/main/java/com/sandordavid/SortingWebapp/Application.java
+++ backend/src/main/java/com/sandordavid/SortingWebapp/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        System.err.println("Hello, logs!");
         SpringApplication.run(Application.class, args);
     }
 }
